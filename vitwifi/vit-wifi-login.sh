1f0acd51552f17eed3257e35b5f02e7d7240ab40
saharshbhansali <saharsh.bhansali@gmail.com>
2022-08-26 01:01:25 +0530

    Updated wifi login file

--- vitwifi/vit-wifi-login.sh
+++ vitwifi/vit-wifi-login.sh
@@ -3,14 +3,8 @@
 ID="21BCI0028"
 PASS="LK3RA6"
 
-echo "Checking for VIT WiFi..."
-
-SSID=$(iw dev | grep -iE 'ssid') # grep -iE 'VIT[2.4 5]+G')
-
-if [[ $SSID =~ 'VIT[2.4 5]+G$' ]]
-then 
+login_request(){
 	echo "Logging in $ID!..."
-
 curl 'http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://www.msftconnecttest.com/redirect' \
   -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8' \
   -H 'Accept-Language: en-IN;q=0.8' \
@@ -23,11 +17,33 @@ curl 'http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://www.msftconnect
   -H 'Sec-GPC: 1' \
   -H 'Upgrade-Insecure-Requests: 1' \
   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36' \
-  --data-raw "userId=$ID&password=$PASS&serviceName=ProntoAuthentication&Submit22=Login" \
+  --data-raw "userId=$1&password=$2&serviceName=ProntoAuthentication&Submit22=Login" \
   --compressed \
-  --insecure -sS &> ~/logs-vit-wifi.txt #/dev/null"
-
+  --insecure -sS &> ~/logs-vit-wifi.txt #/dev/null
 	echo "Logged in $ID"
+}
+
+# All methods of Substring checking
+	#[[ grep -q "success" <<< $Y ]];
+	#[[$Y =~ "successfully"]] <-- error was here, I did not give space between " and ]]
+	#[[ grep -iE "VIT[2.4 5]+G$" ]]
+
+# ALL Self Calling Methods Work
+  # $HOME/login-vit-wifi.sh
+  # ./$*
+  #./$0
+  # a=$(./$0)
+  # /bin/zsh /home/saharsh/"$0"
+  # exec /home/saharsh/login-vit-wifi.sh
+	# login_request $ID $PASS
+
+echo "Checking for VIT WiFi..."
+
+SSID=$(iw dev | grep -iE 'ssid')
+
+if [[ $SSID =~ 'VIT[2.4 5]+G$' ]]
+then 
+  login_request $ID $PASS 
 
 else
 	echo "Wrong Network... "
@@ -36,18 +52,22 @@ else
 
 	Y=$(nmcli device wifi connect "VIT5G")
 
-	if [[! $Y =~ "successfully" ]]
+	if [[ "$Y" =~ 'successfully' ]]
 	then
+	  login_request $ID $PASS
+	
+	else
 		echo "Failed to connect to VIT 5G WiFi, trying 2.4G"
 
 		Y=$(nmcli device wifi connect "VIT2.4G")
-  fi
 
-	if [[$Y =~ "successfully"]]
-	then
-		./login-vit-wifi.sh
-	
-	else
-		echo "Connection attempts failed. Try manually"
+	  if [[ "$Y" =~ "successfully" ]] 
+	  then
+		  
+		  login_request $ID $PASS 
+	  
+	  else
+		  echo "Connection attempts failed. Try manually"
+	  fi
 	fi
 fi
