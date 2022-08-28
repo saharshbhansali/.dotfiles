842458f1e56bfa9c03edfde726bbf66a20d77cbb
saharshbhansali <saharsh.bhansali@gmail.com>
2022-08-29 04:07:54 +0530

    updated autostarted path variable

--- .zsh/zsh-autorun.zsh
+++ .zsh/zsh-autorun.zsh
@@ -1,3 +1,4 @@
+export PATH="/home/saharsh/.nvm/versions/node/v16.17.0/bin:/usr/lib64/ccache:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/var/lib/snapd/snap/bin:/home/saharsh/.local/bin:/home/saharsh/.cargo/bin"
 neofetch
 echo '\nCurrent Directory: '
 pwd
