226d6179cebd06d3db3b66f12eccc0c3badc460a
saharshbhansali <saharsh.bhansali@gmail.com>
2022-09-01 01:23:54 +0530

    Updated vpn connecting script to check for the type of vpn client needed

--- vitwifi/vit-vpn-connect.sh
+++ vitwifi/vit-vpn-connect.sh
@@ -1,3 +1,19 @@
 nm-applet &
+
+# setting default value for the client parameter
+${client:-"proton"}
+
+if [[ $client == "proton" ]]; then
 pkill protonvpn
 protonvpn-cli connect --fastest
+
+elif [[ $client == 'windscribe' ]]; then
+pkill windscribe
+windscribe-cli connect best
+windscribe-cli disconnect
+windscribe-cli connect best
+
+else
+  echo "Chose --client windscribe or proton"
+
+fi
