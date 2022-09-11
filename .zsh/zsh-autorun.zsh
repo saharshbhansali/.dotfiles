5a3454fb32061fd49b7787f4cb0733a0ca344152
saharshbhansali <saharsh.bhansali@gmail.com>
2022-09-12 01:49:03 +0530

    (update) zsh-autorun and kde-shortcuts

--- .zsh/zsh-autorun.zsh
+++ .zsh/zsh-autorun.zsh
@@ -1,8 +1,4 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export PATH="/home/saharsh/.nvm/versions/node/v16.17.0/bin:/usr/lib64/ccache:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/var/lib/snapd/snap/bin:/home/saharsh/.local/bin:/home/saharsh/.cargo/bin"
-
+export PATH="/opt/gradle/gradle-7.5.1/bin:/home/saharsh/.nvm/versions/node/v16.17.0/bin:/usr/lib64/ccache:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/var/lib/snapd/snap/bin:/home/saharsh/.local/bin:/home/saharsh/.cargo/bin"
 neofetch
 echo '\nCurrent Directory: '
 pwd
