fb6a270c402f031349aebf93bd4afba83f4c7af0
saharshbhansali <saharsh.bhansali@gmail.com>
2022-10-06 22:19:59 +0530

    (update) WiFi and vpn fixes

--- vitwifi/vit-wifi-login.sh
+++ vitwifi/vit-wifi-login.sh
@@ -41,8 +41,8 @@ curl 'http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://www.msftconnect
 echo "Checking for VIT WiFi..."
 
 # SSID=$(iw dev $device link | grep -iE 'ssid')
-# SSID=$(iw dev $device link | grep -iE 'VIT(2.4|5)(G|)$')
 # SSID=$(nmcli | grep -iE 'VIT[2.4 5][G]$')
+# SSID=$(nmcli | grep -iE 'VIT(2.4|5)(G|)$')
 SSID=$(iw dev $device link | grep -iE 'VIT(2.4|5)(G|)$')
 echo $SSID
 
