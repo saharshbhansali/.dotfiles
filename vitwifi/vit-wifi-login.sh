caa8936e79062689c880dc2a12a678213f25ca69
saharshbhansali <saharsh.bhansali@gmail.com>
2023-01-29 00:22:46 +0530

    clean: wifi login script
    Signed-off-by: saharshbhansali <saharsh.bhansali@gmail.com>

--- vitwifi/vit-wifi-login.sh
+++ vitwifi/vit-wifi-login.sh
@@ -7,33 +7,6 @@ device=$(read)
 login_request(){
 	echo "Logging in $ID!..."
 	
-	## Old Request
-	# curl 'http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://www.msftconnecttest.com/redirect' \
-  # -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8' \
-  # -H 'Accept-Language: en-IN;q=0.8' \
-  # -H 'Cache-Control: max-age=0' \
-  # -H 'Connection: keep-alive' \
-  # -H 'Content-Type: application/x-www-form-urlencoded' \
-  # -H 'DNT: 1' \
-  # -H 'Origin: http://phc.prontonetworks.com' \
-  # -H 'Referer: http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://networkcheck.kde.org/' \
-  # -H 'Sec-GPC: 1' \
-  # -H 'Upgrade-Insecure-Requests: 1' \
-  # -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36' \
-  # --data-raw "userId=$1&password=$2&serviceName=ProntoAuthentication&Submit22=Login" \
-  # --compressed \
-  # --insecure -sS &> /dev/null # ~/logs-vit-wifi.txt 
-
-	## New Request - from Brave / Chromium
-  # curl 'http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://captive.apple.com/hotspot-detect.html' \
-  # -H 'Content-Type: application/x-www-form-urlencoded' \
-  # -H 'Origin: http://phc.prontonetworks.com' \
-  # -H 'Referer: http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://www.gstatic.com/generate_204' \
-  # -H 'Upgrade-Insecure-Requests: 1' \
-  # -H 'User-Agent: Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
-  # --data-raw "userId=$1password=$2&serviceName=ProntoAuthentication&Submit22=Login" \
-  # --compressed &> /dev/null 
-	
 	## Request from Firefox
   curl 'http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://captive.apple.com/hotspot-detect.html' -X POST -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/109.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: http://phc.prontonetworks.com' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' --data-raw "userId=$1&password=$2&serviceName=ProntoAuthentication&Submit22=Login" &> /dev/null
 	
