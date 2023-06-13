7e115d071ca20b1dc6be5431482b91e66db5ece9
saharshbhansali <saharsh.bhansali@gmail.com>
2023-06-14 00:37:44 +0530

    update: Cloudflare WARP - using comment by Nc0de on https://www.reddit.com/r/Fedora/comments/vj5x6l/easy_way_to_get_cloudflarewarp_client_on_fedora/ wgcf can be used to register and generate a Cloudflare profile:
    wgcf register
    
    wgcf generate
    
    sudo cp ./wgcf-profile.conf /etc/wireguard/cloudflare.conf
    
    Then wireguard-tools (sudo dnf install wireguard-tools) can be used to connect to the 1.1.1.1 (Cloudflare WARP) with the command:
    
    wg-quick up cloudflare
    
    To disconnect:
    
    wg-quick down cloudflare
    
    That's it.
    
    Signed-off-by: saharshbhansali <saharsh.bhansali@gmail.com>

--- vitwifi/vit-vpn-connect.sh
+++ vitwifi/vit-vpn-connect.sh
@@ -1,44 +1,52 @@
+#!/bin/bash
+
 nm-applet &
 
 # setting default value for the client parameter
 ## client="${client:=$default}"
 ## client="${1:-$default}"
 
-default='r'
+default='c'
 client="${1:-$default}"
 
-if [[ $client == 'r' ]];
-then
-  echo "Riseup-VPN starting"
-  pkill riseup
-  
-  # riseup-vpn.launcher --start-vpn "on"
-  # riseup-vpn.launcher --start-vpn "off"
-  # riseup-vpn.launcher -w -a --start-vpn "on" 2&> /dev/null &
-  riseup-vpn.launcher -w -a --start-vpn "on" 2&> /dev/null  
-  curl -H "X-Auth-Token: `cat /tmp/bitmask-token`" localhost:8080/vpn/status                                                                              ─╯
-  curl -H "X-Auth-Token: `cat /tmp/bitmask-token`" localhost:8080/vpn/start
-  speedtest-cli
+if [[ $client == 'c' ]]; then
+	echo "Cloudflare WARP starting"
+	wg-quick down cloudflare
 
-elif [[ $client == 'p' ]]; 
-then
-  echo "Proton VPN starting"
-  pkill protonvpn
-  protonvpn-cli connect --fastest
-  nmcli connection down pvpn-ipv6leak-protection
+	wg-quick up cloudflare
+	speedtest-cli
 
-elif [[ $client == 'w' ]];
-then
-  echo "Windscribe VPN starting"
-  pkill windscribe
-  windscribe-cli connect best
-  windscribe-cli disconnect
-  windscribe-cli connect best
+elif [[ $client == 'r' ]]; then
+	echo "Riseup-VPN starting"
+	pkill riseup
+
+	# riseup-vpn.launcher --start-vpn "on"
+	# riseup-vpn.launcher --start-vpn "off"
+	# riseup-vpn.launcher -w -a --start-vpn "on" 2&> /dev/null &
+	riseup-vpn.launcher -w -a --start-vpn "on" 2 &>/dev/null
+	curl -H "X-Auth-Token: $(cat /tmp/bitmask-token)" localhost:8080/vpn/status ─╯
+	curl -H "X-Auth-Token: $(cat /tmp/bitmask-token)" localhost:8080/vpn/start
+	speedtest-cli
+
+elif [[ $client == 'p' ]]; then
+	echo "Proton VPN starting"
+	pkill protonvpn
+	protonvpn-cli connect --fastest
+	nmcli connection down pvpn-ipv6leak-protection
+	speedtest-cli
+
+elif [[ $client == 'w' ]]; then
+	echo "Windscribe VPN starting"
+	pkill windscribe
+	windscribe-cli connect best
+	windscribe-cli disconnect
+	windscribe-cli connect best
+	speedtest-cli
 
 else
-  echo "Choose --client (r)iseup-vpn (w)indscribe or (p)roton"
+	echo "Choose --client (c)loudflare-warp, (r)iseup-vpn, (w)indscribe or (p)roton"
 
 fi
 
 echo "VPN Script Complete. Starting Ping test"
-# ping -c 5 1.1.1.1
+ping -c 5 1.1.1.1
