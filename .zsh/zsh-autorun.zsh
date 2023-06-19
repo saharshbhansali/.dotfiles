bbd419224a962754be5b96bc33d1b64907a0788e
saharshbhansali <saharsh.bhansali@gmail.com>
2023-06-19 19:45:55 +0530

    update: add nessus into path
    Signed-off-by: saharshbhansali <saharsh.bhansali@gmail.com>

--- .zsh/zsh-autorun.zsh
+++ .zsh/zsh-autorun.zsh
@@ -1,5 +1,6 @@
-export PATH="/home/saharsh/bin:/opt/gradle/gradle-7.5.1/bin:/home/saharsh/.nvm/versions/node/v16.17.0/bin:/usr/lib64/ccache:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/var/lib/snapd/snap/bin:/home/saharsh/.local/bin:/home/saharsh/.cargo/bin"
-
+export PATH="/home/saharsh/bin:/opt/gradle/gradle-7.5.1/bin:/home/saharsh/.nvm/versions/node/v16.17.0/bin:/usr/lib64/ccache:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/var/lib/snapd/snap/bin:/home/saharsh/.local/bin:/home/saharsh/.cargo/bin:/opt/nessus/sbin:/opt/nessus/bin"
+export LD_LIBRARY_PATH=/opt/oracle/instantclient_21_10:$LD_LIBRARY_PATH
+export PATH=$LD_LIBRARY_PATH:$PATH
 # if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ]; then
 #    tmux attach || tmux >/dev/null 2>&1
 # fi
