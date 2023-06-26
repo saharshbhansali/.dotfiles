487e0b2d6e741bc82b8073505cbea0108d4f3ba6
saharshbhansali <saharsh.bhansali@gmail.com>
2023-06-26 21:55:17 +0530

    update: update wifi scripts to modify wifi radio status
    Signed-off-by: saharshbhansali <saharsh.bhansali@gmail.com>

--- vitwifi/vit-wifi-login.zsh
+++ vitwifi/vit-wifi-login.zsh
@@ -1,10 +1,11 @@
 #!/bin/zsh
-echo "Enter your ID, Password and Device Name: "
-read "ID?Username: "
+
+# Data Prompt
+echo "Enter your ID, Password and Device Name"
+read "ID?Registration Number: "
 read -s "PASS?Password: "
 echo
-read "device?Device: "
-
+read "device?Network Device Name: "
 echo "Username: $ID\nPassword: $PASS\nDevice: $device"
 
 login_request(){
@@ -17,71 +18,78 @@ login_request(){
 }
 
 # All methods of Substring checking
-	#[[ grep -q "success" <<< $Y ]];
-	#[[$Y =~ "successfully"]] <-- error was here, I did not give space between " and ]]
-	#[[ grep -iE "VIT[2.4 5]+G$" ]]
+## [[ grep -q "success" <<< $Y ]];
+## [[$Y =~ "successfully"]] <-- error was here, I did not give space between " and ]]
+## [[ grep -iE "VIT[2.4 5]+G$" ]]
 
 # ALL Self Calling Methods Work
-  # $HOME/login-vit-wifi.sh
-  # ./$*
-  #./$0
-  # a=$(./$0)
-  # /bin/zsh /home/saharsh/"$0"
-  # exec /home/saharsh/login-vit-wifi.sh
-	# login_request $ID $PASS
+## $HOME/login-vit-wifi.sh
+## ./$*
+## ./$0
+## a=$(./$0)
+## /bin/zsh /home/saharsh/"$0"
+## exec /home/saharsh/login-vit-wifi.sh
+## login_request $ID $PASS
 
-echo "Turning on WiFi radio..."
+echo "Checking WiFi radio status..."
+WIFI=$(wifi | grep -iE 'off')
+if [[ $WIFI ]]
+then
+  echo "Turning on WiFi radio..."
+  nmcli radio wifi on 
+  sleep 3s
+fi
 
-nmcli radio wifi on 
-
+echo "WiFi radio is on!"
 echo "Checking for VIT WiFi..."
 
-# SSID=$(iw dev $device link | grep -iE 'ssid')
-# SSID=$(nmcli | grep -iE 'VIT[2.4 5][G]$')
-# SSID=$(nmcli | grep -iE 'VIT(2.4|5)(G|)$')
+## SSID=$(iw dev $device link | grep -iE 'ssid')
+## SSID=$(nmcli | grep -iE 'VIT[2.4 5][G]$')
+## SSID=$(nmcli | grep -iE 'VIT(2.4|5)(G|)$')
+
 SSID=$(iw dev $device link | grep -iE 'VIT(2.4|5)(G|)$')
 echo $SSID
 
 if [[ $SSID ]]
-then 
-  login_request $ID $PASS 
+then
+  login_request $ID $PASS
 
 else
 	echo "VIT Network not found, changing connection... "
 
-	echo " Trying to login to VIT 5G WiFi"
+	echo "Trying to login to VIT 2.4G WiFi"
 
-	Y=$(nmcli device wifi connect "VIT5G")
+	Y=$(nmcli device wifi connect "VIT2.4G")
 
 	if [[ "$Y" =~ 'successfully' ]]
 	then
 	  login_request $ID $PASS
 	
 	else
-		echo "Failed to connect to VIT 5G WiFi, trying 2.4G"
+		echo "Failed to connect to VIT 2.4G WiFi, trying 5G"
 
-		Y=$(nmcli device wifi connect "VIT2.4G")
+		Y=$(nmcli device wifi connect "VIT5G")
 
-	  if [[ "$Y" =~ "successfully" ]] 
+	  if [[ "$Y" =~ "successfully" ]]
 	  then
 	  	echo "Logged in to VIT WiFi"
-		  
+		
 		  login_request $ID $PASS 
-	  
+	
 	  else
 		  echo "Connection attempts failed. Try manually"
 	  fi
 	fi
 fi
 
-ipv6=$(nmcli | grep -iE 'pvpn-ipv6leak-protection')
-if [[ $ipv6 ]]
-then 
-	nmcli connection down "pvpn-ipv6leak-protection"
-	echo "Disabled ProtonVPN IPV6 Leak Protection"
-else
-	echo "Done"
-fi
+## ipv6=$(nmcli | grep -iE 'pvpn-ipv6leak-protection')
+## if [[ $ipv6 ]]
+## then
+##   nmcli connection down "pvpn-ipv6leak-protection"
+##   echo "Disabled ProtonVPN IPV6 Leak Protection"
+## else
+##   echo "Done"
+## fi
 
 echo "WiFi Script Complete. Starting Ping test."
 ping -c 5 1.1.1.1
