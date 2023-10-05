3efcdd6f6e40b723f238272b6011393e22b36df1
saharshbhansali <saharsh.bhansali@gmail.com>
2023-10-06 01:42:30 +0530

    Update: sync script
    Signed-off-by: saharshbhansali <saharsh.bhansali@gmail.com>

--- sync.sh
+++ sync.sh
@@ -14,9 +14,9 @@ if [[ ${#gstp} -ne 0 ]]; then
 	echo "Files staged. Committing changes."
 	sleep 10
 	echo $(git commit -m "Automated backup ($timestamp): $gstp")
-	echo "Changes committed. Pulling changes from remote (with rebase strategy)."
+	echo "Changes committed. Pulling changes from remote (with fast-forward strategy)."
 	sleep 10
-	echo $(git pull --rebase)
+	echo $(git pull --ff)
 	echo "Changes pulled. Pushing changes to remote."
 	sleep 10
 	echo $(git push --verbose) #--all)
