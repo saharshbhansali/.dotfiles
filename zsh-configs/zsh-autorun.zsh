5b1f88670c80ab8fa98ae546670196719f36b6c8
saharshbhansali <saharsh.bhansali@gmail.com>
2023-12-26 02:47:11 +0530

    Update: environment exports
    Signed-off-by: saharshbhansali <saharsh.bhansali@gmail.com>

--- zsh-configs/zsh-autorun.zsh
+++ zsh-configs/zsh-autorun.zsh
@@ -1,16 +1,27 @@
-export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/lib64/ccache:$HOME/bin:$HOME/.local/bin:/var/lib/snapd/snap/bin:$HOME/.cargo/bin:"
+## Environmental Variable Setup
 
-# export LD_LIBRARY_PATH="/opt/oracle/instantclient_21_10" # :$LD_LIBRARY_PATH"
+# Gradle Setup
 # GRADLE="/opt/gradle/gradle-7.5.1/bin:"
+
+# Nessus Setup 
 # NESSUS="/opt/nessus:"
-NODE="$HOME/.nvm/versions/node/v16.17.0/bin:"
+
+# GoLang Setup 
+# export GOROOT="/opt/go"
+# export GO_USER="$HOME/go/bin/"
+export GOPATH="/opt/go/bin/"
+GO_ALL="$GOPATH"
+
+# Node Setup
+# NODE="$HOME/.nvm/versions/node/v16.17.0/bin:"
+
+# Set XDG_CONFIG_HOME
 export XDG_CONFIG_HOME="$HOME/.config/"
-#export GOROOT="/opt/go"
-#export GOPATH="/opt/go/bin/"
-#GO_ALL="$GOROOT:$GOPATH:/opt/go/bin/bin"
-#export GO_USER="$HOME/go/bin/"
-#export PATH=$PATH:$XDG_CONFIG_HOME:$GO_ALL
+
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/lib64/ccache:$HOME/bin:$HOME/.local/bin:/var/lib/snapd/snap/bin:$HOME/.cargo/bin:"
+export PATH=$PATH:$XDG_CONFIG_HOME:$GO_ALL
 
 #if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ]; then
 #  tmux attach || tmux >/dev/null 2>&1
 #fi 
+
